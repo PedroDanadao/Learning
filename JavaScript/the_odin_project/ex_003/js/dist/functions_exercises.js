3da4265a326e4cbebaf37a39a7b8a2a1bd4b58ff
PedroDanadao <15200544+PedroDanadao@users.noreply.github.com>
2023-10-02 14:05:37 -0300

    Make another of functions exercise items

--- JavaScript/the_odin_project/ex_003/js/dist/functions_exercises.js
+++ JavaScript/the_odin_project/ex_003/js/dist/functions_exercises.js
@@ -10,9 +10,15 @@ var height_input = document.querySelector("#height_input");
 var color_select = document.querySelector("#colors_select");
 var canvas_area = document.querySelector(".canvas_area");
 var draw_shape_button = document.querySelector(".draw_shape_button");
+var names_list_input = document.querySelector(".names_list_input");
+var random_input_1 = document.querySelector("#random_input_1");
+var random_input_2 = document.querySelector("#random_input_2");
+var choose_name_result_2 = document.querySelector(".choose_name_result_2");
+var choose_name_button_2 = document.querySelector(".choose_name_button_2");
 // make the connections(event listeners) from the html elements to the functions
 choose_name_button === null || choose_name_button === void 0 ? void 0 : choose_name_button.addEventListener("click", choose_name);
 draw_shape_button.addEventListener("click", draw_shape);
+choose_name_button_2.addEventListener("click", choose_name_2);
 // make the functions that will be called when the user interacts with the html elements
 function choose_name() {
     var names = ['Chris', 'Li Kang', 'Anne', 'Francesca', 'Mustafa', 'Tina', 'Bert', 'Jada'];
@@ -43,7 +49,7 @@ function draw_shape() {
         for (var i in nan_list) {
             problematic_inputs += nan_list[i] + "\n";
         }
-        var error_message = "The following inputs don't contain a valid number:\n" + problematic_inputs + "check their values and try again";
+        var error_message = "The following inputs don't contain a valid number:\n" + problematic_inputs + "\ncheck their values and try again";
         window.alert(error_message);
         throw error_message;
     }
@@ -53,11 +59,47 @@ function draw_shape() {
     ctx.fillStyle = color_select.value;
     ctx.fillRect(x_value, y_value, width_value, height_value);
 }
-function assert_number(passed_value) {
-    // makes sure that the value to be returned will be a number
-    var value_number = Number(passed_value);
-    if (isNaN(value_number))
-        throw "the passed value " + passed_value + " is not a number";
-    else
-        return value_number;
+function random_interval(num_1, num_2) {
+    // returns a random number between the maximum and minimum number intervals
+    var max_value = Math.max(num_1, num_2);
+    var min_value = Math.min(num_1, num_2);
+    return Math.floor(Math.random() * (max_value - min_value + 1) + min_value);
+}
+function choose_name_2() {
+    // check if the names list is empty. If so, then alert the user and throw an error
+    if (!names_list_input.value) {
+        window.alert("The names list is empty. Add some names separated by a comma and space and try again");
+        throw "The names list is empty. Add some names separated by a comma and space and try again";
+    }
+    var names_list_string = names_list_input.value;
+    names_list_string = names_list_string.replace(/, /g, "*-*-*-*-*");
+    names_list_string = names_list_string.replace(/,/g, "*-*-*-*-*");
+    var names_list = names_list_string.split("*-*-*-*-*");
+    var random_value_1 = parseInt(random_input_1.value);
+    var random_value_2 = parseInt(random_input_2.value);
+    if (isNaN(random_value_1)) {
+        window.alert("The value of the first random number is not valid. Type a valid number for both and try again");
+        throw "The value of the first random number is not valid. Type a valid number for both and try again";
+    }
+    if (isNaN(random_value_2)) {
+        window.alert("The value of the second random number is not valid. Type a valid number for both and try again");
+        throw "The value of the second random number is not valid. Type a valid number for both and try again";
+    }
+    // get the maximum value of both random values and the minimum value between the max and the length of the names list
+    var max_random = Math.max(random_value_1, random_value_2);
+    max_random = Math.min(names_list.length, max_random);
+    var min_random = Math.min(random_value_1, random_value_2);
+    min_random = Math.max(min_random, 1);
+    // get the random number between the specified numbers and print the name in the position of the resulting random number
+    var generated_random = random_interval(min_random, max_random);
+    choose_name_result_2.innerText = names_list[generated_random - 1];
+}
+function removeValue(value, index, arr) {
+    // If the value at the current array index matches the specified value (2)
+    if (value === 1) {
+        // Removes the value from the original array
+        arr.splice(index, 1);
+        return true;
+    }
+    return false;
 }
--- JavaScript/the_odin_project/ex_003/js/functions_exercises.ts
+++ JavaScript/the_odin_project/ex_003/js/functions_exercises.ts
@@ -13,9 +13,18 @@ const color_select = document.querySelector("#colors_select") as HTMLSelectEleme
 const canvas_area = document.querySelector(".canvas_area") as HTMLCanvasElement;
 const draw_shape_button = document.querySelector(".draw_shape_button") as HTMLInputElement;
 
+const names_list_input = document.querySelector(".names_list_input") as HTMLInputElement;
+const random_input_1 = document.querySelector("#random_input_1") as HTMLInputElement;
+const random_input_2 = document.querySelector("#random_input_2") as HTMLInputElement;
+const choose_name_result_2 = document.querySelector(".choose_name_result_2") as HTMLDivElement;
+const choose_name_button_2 = document.querySelector(".choose_name_button_2") as HTMLInputElement;
+
 // make the connections(event listeners) from the html elements to the functions
 choose_name_button?.addEventListener("click", choose_name);
+
 draw_shape_button.addEventListener("click", draw_shape);
+
+choose_name_button_2.addEventListener("click", choose_name_2);
 
 // make the functions that will be called when the user interacts with the html elements
 function choose_name() {
@@ -64,12 +73,58 @@ function draw_shape() {
     ctx.fillRect(x_value, y_value, width_value, height_value);
 }
 
-function assert_number(passed_value) {
-    // makes sure that the value to be returned will be a number
-    let value_number = Number(passed_value);
 
-    if (isNaN(value_number))
-        throw `the passed value ${passed_value} is not a number`;
-    else
-        return value_number
+function random_interval(num_1: number, num_2: number) {
+    // returns a random number between the maximum and minimum number intervals
+    const max_value = Math.max(num_1, num_2);
+    const min_value = Math.min(num_1, num_2);
+
+    return Math.floor(Math.random() * (max_value - min_value + 1) + min_value)
+}
+
+function choose_name_2() {
+    // check if the names list is empty. If so, then alert the user and throw an error
+    if (! names_list_input.value){
+        window.alert("The names list is empty. Add some names separated by a comma and space and try again");
+        throw "The names list is empty. Add some names separated by a comma and space and try again";
+    }
+
+    let names_list_string = names_list_input.value;
+    names_list_string = names_list_string.replace(/, /g, "*-*-*-*-*");
+    names_list_string = names_list_string.replace(/,/g, "*-*-*-*-*");
+    const names_list = names_list_string.split("*-*-*-*-*");
+    names_list.filter(removeValue);
+
+    const random_value_1 = parseInt(random_input_1.value);
+    const random_value_2 = parseInt(random_input_2.value);
+
+    if (isNaN(random_value_1)) {
+        window.alert(`The value of the first random number is not valid. Type a valid number for both and try again`);
+        throw `The value of the first random number is not valid. Type a valid number for both and try again`;
+    }
+    
+    if (isNaN(random_value_2)) {
+        window.alert(`The value of the second random number is not valid. Type a valid number for both and try again`);
+        throw `The value of the second random number is not valid. Type a valid number for both and try again`;   
+    }
+
+    // get the maximum value of both random values and the minimum value between the max and the length of the names list
+    let max_random = Math.max(random_value_1, random_value_2);
+    max_random = Math.min(names_list.length, max_random);
+    let min_random = Math.min(random_value_1, random_value_2);
+    min_random = Math.max(min_random, 1);
+
+    // get the random number between the specified numbers and print the name in the position of the resulting random number
+    const generated_random = random_interval(min_random, max_random);
+    choose_name_result_2.innerText = names_list[generated_random - 1];
+}
+
+function removeValue(value: string, index: number, arr) {
+    // If the value at the current array index matches the specified value (2)
+    if (value === '') {
+    // Removes the value from the original array
+        arr.splice(index, 1);
+        return true;
+    }
+    return false;
 }
