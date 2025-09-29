5d18799eb0865b5a67a6224a33154ad7a800c7b6
PedroDanadao <15200544+PedroDanadao@users.noreply.github.com>
2025-09-28 21:21:39 -0300

    Make the Json lesson

---
+++ JavaScript/the_odin_project/Javascript Course/ex_008_JSON/js/dist/mdn_json.js
@@ -0,0 +1,113 @@
+async function populate() {
+    const request_url = "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
+    const request = new Request(request_url);
+    const response = await fetch(request);
+    const super_heroes_string = await response.text();
+    const super_heroes = JSON.parse(super_heroes_string);
+    // console.log(super_heroes);
+    populate_header(super_heroes);
+    populate_heroes(super_heroes);
+}
+populate();
+function populate_header(super_heroes) {
+    const header = document.querySelector("header");
+    const my_h1 = document.createElement("h1");
+    my_h1.textContent = super_heroes.squadName;
+    header.appendChild(my_h1);
+    const my_para = document.createElement("p");
+    my_para.textContent = `Hometown: ${super_heroes.homeTown} // Formed: ${super_heroes.formed}`;
+    header.appendChild(my_para);
+}
+function populate_heroes(super_heroes) {
+    const section = document.querySelector("section");
+    const heroes = super_heroes.members;
+    for (const hero of heroes) {
+        const my_article = document.createElement("article");
+        const my_h2 = document.createElement("h2");
+        const my_para = document.createElement("p");
+        const my_para_2 = document.createElement("p");
+        const my_para_3 = document.createElement("p");
+        const my_list = document.createElement("ul");
+        my_h2.textContent = hero.name;
+        my_para.textContent = `Secret Identity: ${hero.secretIdentity}`;
+        my_para_2.textContent = `Age: ${hero.age}`;
+        my_para_3.textContent = "Super Powers:";
+        const super_powers = hero.powers;
+        for (const power of super_powers) {
+            const list_item = document.createElement("li");
+            list_item.textContent = power;
+            my_list.appendChild(list_item);
+        }
+        my_article.appendChild(my_h2);
+        my_article.appendChild(my_para);
+        my_article.appendChild(my_para_2);
+        my_article.appendChild(my_para_3);
+        my_article.appendChild(my_list);
+        section.appendChild(my_article);
+    }
+}
+let my_obj = { name: "John Connor", age: 14 };
+let my_string = JSON.stringify(my_obj);
+console.log(my_string);
+// const super_heroes = {
+//     "squadName": "Super hero squad",
+//     "homeTown": "Metro City",
+//     "formed": 2016,
+//     "secretBase": "Super tower",
+//     "active": true,
+//     "members": [
+//         {
+//         "name": "Molecule Man",
+//         "age": 29,
+//         "secretIdentity": "Dan Jukes",
+//         "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+//         },
+//         {
+//             "name": "Madame Uppercut",
+//             "age": 39,
+//             "secretIdentity": "Jane Wilson",
+//             "powers": [
+//                 "Million tonne punch",
+//                 "Damage resistance",
+//                 "Superhuman reflexes"
+//             ]
+//         },
+//         {
+//             "name": "Eternal Flame",
+//             "age": 1000000,
+//             "secretIdentity": "Unknown",
+//             "powers": [
+//                 "Immortality",
+//                 "Heat Immunity",
+//                 "Inferno",
+//                 "Teleportation",
+//                 "Interdimensional travel"
+//             ]
+//         }
+//     ]
+// }
+// console.log(super_heroes.homeTown);
+// console.log(super_heroes.members[1].powers[2]);
+// const second_list = [
+//     {
+//         "name": "Molecule Man",
+//         "age": 29,
+//         "secretIdentity": "Dan Jukes",
+//         "powers": [
+//             "Radiation resistance",
+//             "Turning tiny",
+//             "Radiation blast"
+//         ]
+//     },
+//     {
+//         "name": "Madame Uppercut",
+//         "age": 39,
+//         "secretIdentity": "Jane Wilson",
+//         "powers": [
+//             "Million tonne punch",
+//             "Damage resistance",
+//             "Superhuman reflexes"
+//         ]
+//     }
+// ]
+// console.log(second_list[0].powers[0])
---
+++ JavaScript/the_odin_project/Javascript Course/ex_008_JSON/js/w3schools.ts
@@ -0,0 +1,76 @@
+const obj_string = '{"name":"John", "age":30, "city":"New York"}';
+const obj_object = JSON.parse(obj_string);
+
+console.log(obj_object);
+
+const demo_element = document.getElementById("demo") as HTMLParagraphElement;
+demo_element.innerHTML = obj_object.name;
+
+
+const array_str = '["Ford", "BMW", "Audi", "Fiat"]';
+const array_obj = JSON.parse(array_str);
+
+console.log(array_obj);
+
+const date_str = '{"name": "John", "birth": "1968-12-14", "city": "New York"}';
+const date_obj = JSON.parse(date_str);
+date_obj.birth = new Date(date_obj.birth);
+
+// or
+
+// const date_obj = JSON.parse(date_str, function(key, value) {
+//     if (key == "birth") {
+//         return new Date(value);
+//     } 
+//     else {
+//         return value;
+//     }
+// });
+
+demo_element.innerHTML = date_obj.name + ", " + date_obj.birth;
+
+
+const function_str = '{"name": "John", "age": "function () {return 30;}", "city": "New York"}'
+const function_obj = JSON.parse(function_str);
+function_obj.age = eval("(" + function_obj.age + ")");
+
+demo_element.innerHTML = function_obj.name + ", " + function_obj.age();
+
+
+const person_obj = {name: "John", age: 30, city: "New York"};
+const string_person = JSON.stringify(person_obj);
+
+const people = ["John", "Peter", "James", "Mathew"];
+const string_people = JSON.stringify(people);
+
+// Storing Data
+const person_2_obj = {name: "John", age: 31, city: "New York"};
+const string_person_2 = JSON.stringify(person_2_obj);
+localStorage.setItem("testJson", string_person_2);
+
+// Retrieving Data
+let text_got = localStorage.getItem("testJson");
+let obj_got = JSON.parse(text_got);
+console.log(`obj_got: ${obj_got.age}`);
+
+const num = 123e-5;
+const stringfyed = JSON.stringify(num);
+console.log(stringfyed);
+
+const my_bool = true;
+const stringfyed_bool = JSON.stringify(my_bool);
+console.log(stringfyed_bool);
+
+const dated_obj = {"name": "John", "today": new Date(), "city": "New York"};
+const stringfyed_date = JSON.stringify(dated_obj);
+console.log(stringfyed_date);
+
+const functioned_obj = {"name": "John", "age": function() {return 40;}, "city": "New York"};
+const stringfyed_function = JSON.stringify(functioned_obj);
+console.log(stringfyed_function);
+
+const functioned_obj_2 = {"name": "John", "age": function() {return 40;}, "city": "New York"};
+functioned_obj_2.age = functioned_obj_2.age.toString();
+const stringfyed_function_2 = JSON.stringify(functioned_obj_2);
+console.log(stringfyed_function_2);
+
